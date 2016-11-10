f4445103c791669196139f7a1d0b1bc51d5a1abb
mgrinthal <mgrinthal@users.noreply.github.com>
2016-11-10 17:38:45 -0500

    Componentize, add state and props

---
+++ src/js/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = { title: "Welcome!" };
+	}
+
+	render() {
+		return (
+			<div>
+				<Header title={this.state.title}/>
+				<Footer />
+			</div>
+		);
+	}
+}
